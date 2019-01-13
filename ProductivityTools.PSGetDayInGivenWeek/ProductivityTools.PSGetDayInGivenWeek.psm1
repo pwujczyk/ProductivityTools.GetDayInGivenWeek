7591f683e8e59d3d06513316919ac7aac5d2aa15
pwujczyk <pwujczyk@ecovadis.com>
2019-01-13 08:34:31 +0100

    rename whole solutoion

---
+++ ProductivityTools.PSGetDayInGivenWeek/ProductivityTools.PSGetDayInGivenWeek.psm1
@@ -0,0 +1,82 @@
+function RequestedDayOfWeekAlreadyDefined([string]$requestedDayOfWeek,[bool]$dayPresent,[string]$newRequestedDayOfWeek)
+{
+	if ($dayPresent)
+	{
+		if($requestedDayOfWeek -ne '')
+		{
+			throw "Please define only one day"
+		}
+		return $newRequestedDayOfWeek
+	}
+	else
+	{
+		return $requestedDayOfWeek
+	}
+}
+
+function GetRequestedDayOfWeek{
+		[CmdletBinding()]
+		param([Switch]$Monday,
+			[Switch]$Tuesday,
+			[Switch]$Wednesday,
+			[Switch]$Thursday,
+			[Switch]$Friday,
+			[Switch]$Saturday,
+			[Switch]$Sunday)
+			
+			[string]$requestedDayOfWeek="";
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Monday.IsPresent "Monday"
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Tuesday.IsPresent "Tuesday"
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Wednesday.IsPresent "Wednesday"
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Thursday.IsPresent "Thursday"
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Friday.IsPresent "Friday"
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Saturday.IsPresent "Saturday"
+			$requestedDayOfWeek=RequestedDayOfWeekAlreadyDefined $requestedDayOfWeek $Sunday.IsPresent "Sunday"
+			return $requestedDayOfWeek
+		}
+
+function Get-DayInGivenWeek {
+	[CmdletBinding()]
+	param([DateTime]$Date,
+		[Switch]$Monday,
+		[Switch]$Tuesday,
+		[Switch]$Wednesday,
+		[Switch]$Thursday,
+		[Switch]$Friday,
+		[Switch]$Saturday,
+		[Switch]$Sunday,
+
+		[Switch]$Before,
+		[Switch]$After
+		)
+	if($date -eq $null)
+	{
+		throw "Missing date paramter"
+	}
+	
+	[string]$dayProvided=GetRequestedDayOfWeek -Monday:$Monday -Tuesday:$Tuesday -Wednesday:$Wednesday -Thursday:$Thursday -Friday:$Friday -Saturday:$Saturday -Sunday:$Sunday
+	if ($dayProvided -eq "")
+	{
+		throw "Missing day parameter (Monday, Tuesday,...)"
+	}
+	
+	if ($Before.IsPresent)
+	{
+		$direction=-1;
+	}
+	else
+	{
+		$direction=1;
+	}
+	
+	for($i=1; $i -le 7;$i=$i+$direction)
+	{
+		if($date.AddDays($i).DayOfWeek -eq $dayProvided)
+		{
+			$date.AddDays($i)
+			break
+		}
+	}
+}
+
+Export-ModuleMember Get-DayInGivenWeek
---
+++ ProductivityTools.PSGetDayInGivenWeek/Runner.ps1
@@ -0,0 +1,23 @@
+clear
+cd $PSScriptRoot
+Import-Module .\ProductivityTools.PSGetDayInGivenWeek.psm1 -Force
+
+
+Get-DayInGivenWeek $(Get-Date) -Monday -Before 
+Get-DayInGivenWeek $(Get-Date) -Tuesday -Before 
+Get-DayInGivenWeek $(Get-Date) -Wednesday -Before 
+Get-DayInGivenWeek $(Get-Date) -Thursday -Before 
+Get-DayInGivenWeek $(Get-Date) -Friday -Before 
+Get-DayInGivenWeek $(Get-Date) -Saturday -Before 
+Get-DayInGivenWeek $(Get-Date) -Sunday -Before 
+
+Get-DayInGivenWeek $(Get-Date) -Monday -After
+Get-DayInGivenWeek $(Get-Date) -Tuesday -After 
+Get-DayInGivenWeek $(Get-Date) -Wednesday -After 
+Get-DayInGivenWeek $(Get-Date) -Thursday -After 
+Get-DayInGivenWeek $(Get-Date) -Friday -After 
+Get-DayInGivenWeek $(Get-Date) -Saturday -After 
+Get-DayInGivenWeek $(Get-Date) -Sunday -After 
+
+Write-Host "empty"
+Get-DayInGivenWeek $(Get-Date)
